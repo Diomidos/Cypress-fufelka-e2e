c6264a35ad356d5ac055af01b1c0cbe2b947bf03
[React-Hooks] <[ruslanprivozin277@gmail.com]>
2025-12-13 17:41:07 +0300

    мелкие правки 2

--- cypress/support/credentials.js
+++
@@ -1,4 +0,0 @@
-export const TEST_USER = {
-  email: Cypress.env('TEST_USERNAME') || 'Ochko228',
-  password: Cypress.env('TEST_PASSWORD') || 'Zxcvbn',
-}
--- cypress/support/secretKey.js
+++
@@ -1,5 +0,0 @@
-export function setSecretKey() {
-  cy.window().then((win) => {
-    win.localStorage.setItem('sk', 'YJyKb5bbs0XkjR5DmPrD')
-  })
-}
